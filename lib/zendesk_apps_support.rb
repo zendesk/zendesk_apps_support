296537ce30a91671929b4ba6e5640380c0e7e331
Travers McInerney <travers@mcinerney.me>
2014-02-03 13:30:56 -0800

    Line some stuff up

--- lib/zendesk_apps_support.rb
+++ lib/zendesk_apps_support.rb
@@ -3,12 +3,12 @@ module ZendeskAppsSupport
   require 'zendesk_apps_support/sass_functions'
   require 'zendesk_apps_support/engine'
 
-  autoload :AppFile,    'zendesk_apps_support/app_file'
-  autoload :BuildTranslation, 'zendesk_apps_support/build_translation'
-  autoload :I18n,       'zendesk_apps_support/i18n'
-  autoload :Package,    'zendesk_apps_support/package'
-  autoload :AppVersion,    'zendesk_apps_support/app_version'
-  autoload :StylesheetCompiler,    'zendesk_apps_support/stylesheet_compiler'
+  autoload :AppFile,                'zendesk_apps_support/app_file'
+  autoload :BuildTranslation,       'zendesk_apps_support/build_translation'
+  autoload :I18n,                   'zendesk_apps_support/i18n'
+  autoload :Package,                'zendesk_apps_support/package'
+  autoload :AppVersion,             'zendesk_apps_support/app_version'
+  autoload :StylesheetCompiler,     'zendesk_apps_support/stylesheet_compiler'
 
   module Validations
     autoload :ValidationError,       'zendesk_apps_support/validations/validation_error'
