421dbc313829ff5c1dd4711b73bea47a0a385555
Adam Malcontenti-Wilson <amalcontenti-wilson@zendesk.com>
2014-04-04 13:54:12 +1100

    Extract fixture path into separate method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,10 @@ require 'i18n'
 
 I18n.enforce_available_locales = false
 
+def fixture_path(file)
+  "#{File.dirname(__FILE__)}/validations/fixture/#{file}"
+end
+
 def read_fixture_file(file)
-  File.read("#{File.dirname(__FILE__)}/validations/fixture/#{file}")
+  File.read(fixture_path(file))
 end
