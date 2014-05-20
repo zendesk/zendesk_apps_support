c4ea578c464286137a0e430becad6469c381f270
Adam Malcontenti-Wilson <amalcontenti-wilson@zendesk.com>
2014-05-20 14:28:45 +1000

    Relax jshint when using [] notation unnecessarily

--- spec/validations/source_spec.rb
+++ spec/validations/source_spec.rb
@@ -38,6 +38,14 @@ describe ZendeskAppsSupport::Validations::Source do
     errors.first.to_s.should eql "JSHint error in app.js: \n  L1: Missing semicolon."
   end
 
+  it 'should not have a jslint error when using [] notation unnecessarily' do
+    source = mock('AppFile', :relative_path => 'app.js', :read => "var a = {}; a['b'] = 0;")
+    package = mock('Package', :root => '.', :files => [source], :lib_files => [], :requirements_only => false)
+    errors = ZendeskAppsSupport::Validations::Source.call(package)
+
+    errors.should be_nil
+  end
+
   it 'should have a jslint error when missing semicolon in lib js file' do
     package = ZendeskAppsSupport::Package.new('spec/invalid_app')
     errors  = ZendeskAppsSupport::Validations::Source.call(package)
