39edeb985d1a15e0734be167014f0c55b2c7fe93
Olaf Kwant <olaf+github@zendesk.com>
2016-11-08 15:40:36 +1100

    allow lotus box as a location

--- lib/zendesk_apps_support/product.rb
+++ lib/zendesk_apps_support/product.rb
@@ -16,10 +16,12 @@ module ZendeskAppsSupport
     # The product codes below match the values in the database, do not change them!
     PRODUCTS_AVAILABLE = [
       Product.new(code: 1, name: 'support', legacy_name: 'zendesk'),
-      Product.new(code: 2, name: 'chat', legacy_name: 'zopim')
+      Product.new(code: 2, name: 'chat', legacy_name: 'zopim'),
+      Product.new(code: 3, name: 'standalone_chat', legacy_name: 'lotus_box')
     ].freeze
 
     SUPPORT = find_by!(name: 'support')
-    CHAT    = find_by!(name: 'chat')
+    CHAT = find_by!(name: 'chat')
+    STANDALONE_CHAT = find_by!(name: 'standalone_chat')
   end
 end
