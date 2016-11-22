a68920bace76b085b264836df0ee5c330d8adc52
Adam Malcontenti-Wilson <amalcontenti-wilson@zendesk.com>
2016-11-22 14:18:47 +1100

    Accidentally refactor wrong spec

--- spec/validations/marketplace_spec.rb
+++ spec/validations/marketplace_spec.rb
@@ -2,58 +2,48 @@
 require 'spec_helper'
 
 describe ZendeskAppsSupport::Validations::Marketplace do
+  let(:manifest_extras) { {} }
+  let(:manifest) do
+    manifest_json = File.read('spec/fixtures/iframe_only_app/manifest.json')
+    manifest_hash = JSON.parse(manifest_json, symbolize_names: true).merge(manifest_extras)
+    ZendeskAppsSupport::Manifest.new(JSON.dump(manifest_hash))
+  end
+  let(:package) { ZendeskAppsSupport::Package.new('spec/fixtures/iframe_only_app') }
   let(:errors) { ZendeskAppsSupport::Validations::Marketplace.call(package) }
+
+  before do
+    allow(package).to receive(:manifest).and_return(manifest)
+  end
 
   context 'with package with symlinks in it' do
     let(:package) { ZendeskAppsSupport::Package.new('spec/fixtures/symlinks') }
 
-    it 'should raise error when symlink exists inside the app for the marketplace' do
+    it 'raises symlink_in_zip error' do
       expect(errors.first.key).to eq(:symlink_in_zip)
     end
   end
 
-  context 'with package with non-whitelisted experiments' do
-    let(:manifest) { ZendeskAppsSupport::Manifest.new(JSON.dump(manifest_hash)) }
-    let(:manifest_hash) do
-      json = JSON.parse(File.read('spec/fixtures/iframe_only_app/manifest.json'))
-      json['experiments'] = { explodingButtons: true, punnyTickets: true }
-      json
-    end
-    let(:package) do
-      package = ZendeskAppsSupport::Package.new('spec/fixtures/iframe_only_app')
-      allow(package).to receive(:manifest).and_return(manifest)
-      package
-    end
-
-    it 'raises invalid_experiment validation error' do
-      expect(errors[0].key).to eq(:invalid_experiment)
-      expect(errors[0].message).to match(/unavailable experiment: explodingButtons/)
-      expect(errors[1].message).to match(/unavailable experiment: punnyTickets/)
-    end
-  end
-
-  context 'with the default fixture' do
-    let(:package) { ZendeskAppsSupport::Package.new('spec/fixtures/iframe_only_app') }
-    it 'works' do
+  context 'with the iframe_only_app package' do
+    it 'has no errors' do
       expect(errors).to be_empty
     end
-  end
 
-  context 'with package with whitelisted experiements' do
-    let(:package) { ZendeskAppsSupport::Package.new('spec/fixtures/iframe_only_app') }
-    let(:manifest) { ZendeskAppsSupport::Manifest.new(JSON.dump(manifest_hash)) }
-    let(:manifest_hash) do
-      json = JSON.parse(File.read('spec/fixtures/iframe_only_app/manifest.json'))
-      json['experiments'] = { hashParams: true }
-      json
+    context 'with package with non-whitelisted experiments' do
+      let(:manifest_extras) { { experiments: { explodingButtons: true, punnyTickets: true } } }
+
+      it 'raises invalid_experiment validation error' do
+        expect(errors[0].key).to eq(:invalid_experiment)
+        expect(errors[0].message).to match(/unavailable experiment: explodingButtons/)
+        expect(errors[1].message).to match(/unavailable experiment: punnyTickets/)
+      end
     end
-    let(:package) do
-      package = ZendeskAppsSupport::Package.new('spec/fixtures/iframe_only_app')
-      allow(package).to receive(:manifest).and_return(manifest)
-      package
-    end
-    it 'works' do
-      expect(errors).to be_empty
+
+    context 'with whitelisted experiements' do
+      let(:manifest_extras) { { experiments: { hashParams: true } } }
+
+      it 'has no errors' do
+        expect(errors).to be_empty
+      end
     end
   end
 end
