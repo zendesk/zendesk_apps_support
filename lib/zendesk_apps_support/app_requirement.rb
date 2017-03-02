563ec611413268663f25a623f1432b5986b18916
Farhan Rahman <frahman@zendesk.com>
2017-03-02 12:18:16 +1100

    Add organization_fields as a valid requirements resource type

--- lib/zendesk_apps_support/app_requirement.rb
+++ lib/zendesk_apps_support/app_requirement.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module ZendeskAppsSupport
   class AppRequirement
-    TYPES = %w(automations channel_integrations macros targets views ticket_fields triggers user_fields).freeze
+    TYPES = %w(automations channel_integrations macros targets views ticket_fields triggers user_fields organization_fields).freeze
   end
 end
