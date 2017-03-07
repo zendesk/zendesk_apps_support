9193d184446cace0f290a8f1eccf84436776c8f3
Farhan Rahman <frahman@zendesk.com>
2017-03-07 12:02:23 +1100

    Align array elements for Rubocop

--- lib/zendesk_apps_support/app_requirement.rb
+++ lib/zendesk_apps_support/app_requirement.rb
@@ -2,7 +2,7 @@
 module ZendeskAppsSupport
   class AppRequirement
     TYPES = %w(automations channel_integrations
-    macros targets views ticket_fields triggers
-    user_fields organization_fields).freeze
+               macros targets views ticket_fields triggers
+               user_fields organization_fields).freeze
   end
 end
