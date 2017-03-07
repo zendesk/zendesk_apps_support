6c06d486906d5f056800af639efff17f26b58afc
Farhan Rahman <frahman@zendesk.com>
2017-03-07 11:30:46 +1100

    Split collection over lines to fix line length limit

--- lib/zendesk_apps_support/app_requirement.rb
+++ lib/zendesk_apps_support/app_requirement.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 module ZendeskAppsSupport
   class AppRequirement
-    TYPES = %w(automations channel_integrations macros targets views ticket_fields triggers user_fields organization_fields).freeze
+    TYPES = %w(automations channel_integrations
+    macros targets views ticket_fields triggers
+    user_fields organization_fields).freeze
   end
 end
