c25a9eac7f3525722c348b2931e737f6f35f7429
Nick Freeland <nfreeland@zendesk.com>
2017-06-26 11:00:40 +1000

    Address comment

--- lib/zendesk_apps_support/validations/manifest.rb
+++ lib/zendesk_apps_support/validations/manifest.rb
@@ -292,7 +292,7 @@ module ZendeskAppsSupport
         end
 
         def invalid_version_error(manifest, package)
-          valid_to_serve = AppVersion::TO_BE_SERVED.compact
+          valid_to_serve = AppVersion::TO_BE_SERVED
           target_version = manifest.framework_version
 
           if target_version == AppVersion::DEPRECATED
