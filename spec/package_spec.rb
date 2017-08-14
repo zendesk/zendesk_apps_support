d001035e72d57f8f038a7be1cf3c7556c607ec0c
Sean Goedecke <sgoedecke@zendesk.com>
2017-08-15 09:20:34 +1000

    more fixtures - repair package spec

--- spec/package_spec.rb
+++ spec/package_spec.rb
@@ -227,7 +227,13 @@ describe ZendeskAppsSupport::Package do
       let(:manifest) { super().merge('defaultLocale' => nil) }
 
       it 'returns translations' do
-        expected_translations = { 'en' => { 'app' => { 'short_description' => description }, 'custom1' => custom1 } }
+        expected_translations = { 'en' => { 'app' => {
+            'short_description' => description,
+            'description' => 'Access bookmarks',
+            'long_description' => 'Access bookmarks in your Zendesk',
+            'installation_instructions' => 'Pull the big lever'
+          },
+          'custom1' => custom1 } }
         expect(package.send(:translations)).to eq(expected_translations)
         expect(package.locales).to eq(['en'])
       end
