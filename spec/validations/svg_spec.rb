2ed1d3590c0f9761c80fc16b71facc53b1bff70c
Farhan Rahman <frahman@zendesk.com>
2017-09-18 11:38:46 +1000

    Add a test with a leading doctype

--- spec/validations/svg_spec.rb
+++ spec/validations/svg_spec.rb
@@ -30,7 +30,7 @@ stroke="currentColor" stroke-linejoin="round" stroke-width="2" d="M4 8l9-5 9 5v9
 
   context 'non-suspicious but superfluous markup' do
     superfluous_markup = [
-      # markup of the logo in demo apps
+      # markup containing a leading XML declaration
       %(<?xml version="1.0" encoding="utf-8"?>
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" \
 y="0px"
@@ -42,6 +42,17 @@ y="0px"
   c0,0.3-0.2,0.7-0.5,0.9L10.5,17.9z"/>
 <path id="Fill-1" d="M2.2,3.7c-0.3,0.2-0.3,0.4,0,0.6l6.2,3.6C8.7,8,9.2,8,9.4,7.9l6.3-3.6c0.3-0.2,0.3-0.4,0-0.6L9.5,0.1
   C9.2,0,8.8,0,8.5,0.1L2.2,3.7z"/>
+</svg>),
+      # markup containing a doctype
+      %(<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" \
+y="0px"
+   width="14px" height="11px" viewBox="-1 -1 14 11" xml:space="preserve">
+<path d="M11.7,0.3C11.4,0.1,11.1,0,10.8,0c-0.3,0-0.6,0.1-0.8,0.2l0,0l-5.8,6L1.9,3.9C1.7,3.7,1.4,3.6,1.1,3.6S0.6,3.7,\
+0.3,3.9
+  C0.1,4.1,0,4.4,0,4.7C0,5,0.1,5.3,0.3,5.5l3.1,3.2C3.8,9,4.1,9,4.2,9C4.6,9,4.9,8.9,5,8.7l6.6-6.7C12.1,1.5,12.1,0.8,\
+  11.7,0.3z"
+  fill="#78a300" />
 </svg>)
     ]
 
