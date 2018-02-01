456344f79e87226382d2fc60b9d9f88cd37ba2d2
Mehul Porwal <mporwal@zendesk.com>
2018-02-01 12:15:23 +0800

    Reorder attributes

--- lib/zendesk_apps_support/installation.rb
+++ lib/zendesk_apps_support/installation.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module ZendeskAppsSupport
   class Installation
-    attr_accessor :id, :app_id, :app_name, :requirements, :settings, :enabled, :updated_at, :created_at, :collapsible
+    attr_accessor :id, :app_id, :app_name, :requirements, :settings, :enabled, :collapsible, :updated_at, :created_at
 
     def initialize(options)
       options.each do |k, v|
