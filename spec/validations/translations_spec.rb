6864eb88741b8352e169084d109745d862ccfd91
Frederick Zhang <frederick.zhang@zendesk.com>
2023-03-08 15:59:35 +1100

    Fix validating Filipino translation
    Locale identifier of Filipino language is 'fil', which doesn't match the
    old regular expression.
    
    Also added a test to make sure other languages work as well.

--- spec/validations/translations_spec.rb
+++ spec/validations/translations_spec.rb
@@ -52,6 +52,64 @@ describe ZendeskAppsSupport::Validations::Translations do
     end
   end
 
+  context 'when translations for all languages are present' do
+    let(:translation_files) do
+      languages = ["af", "af-za", "ajp-ps", "am", "apc-ps", "ar", "ar-ae",
+                   "ar-BH", "ar-eg", "ar-IL", "ar-KW", "ar-LB", "ar-MA",
+                   "ar-OM", "ar-ps", "ar-QA", "ar-SA", "as-in", "ay-bo", "az",
+                   "be", "bg", "bg-bg", "bn", "bn-in", "bs", "ca", "ca-es",
+                   "cs", "cs-cz", "cy", "da", "da-dk", "de", "de-at", "de-be",
+                   "de-ch", "de-de", "de-dk", "de-it", "de-lu", "de-ro", "el",
+                   "el-cy", "el-gr", "en-001", "en-150", "en-ae", "en-am",
+                   "en-at", "en-au", "en-az", "en-BA", "en-be", "en-bg",
+                   "en-bh", "en-bo", "en-BZ", "en-ca", "en-ch", "en-co",
+                   "en-cr", "en-cy", "en-cz", "en-DE", "en-dk", "en-ec",
+                   "en-ee", "en-eg", "en-es", "en-FI", "en-FR", "en-gb",
+                   "en-ge", "en-GH", "en-gi", "en-gr", "en-gu", "en-hk",
+                   "en-hn", "en-HR", "en-hu", "en-id", "en-ie", "en-il",
+                   "en-in", "en-is", "en-it", "en-jp", "en-KE", "en-kr",
+                   "en-KW", "en-kz", "en-lb", "en-lr", "en-lt", "en-lu",
+                   "en-lv", "en-MA", "en-ME", "en-mt", "en-mx", "en-my",
+                   "en-nl", "en-no", "en-nz", "en-om", "en-pe", "en-ph",
+                   "en-pk", "en-pl", "en-pr", "en-ps", "en-pt", "en-qa",
+                   "en-ro", "en-RS", "en-ru", "en-RW", "en-SA", "en-se",
+                   "en-sg", "en-SI", "en-sk", "en-th", "en-TN", "en-TR",
+                   "en-tw", "en-ua", "en-UG", "en-US", "en-vn", "en-za", "es",
+                   "es-001", "es-419", "es-ar", "es-bo", "es-cl", "es-co",
+                   "es-cr", "es-DO", "es-ec", "es-es", "es-GT", "es-hn",
+                   "es-mx", "es-NI", "es-PA", "es-pe", "es-pr", "es-PY",
+                   "es-SV", "es-us", "es-UY", "es-ve", "et", "et-ee", "eu",
+                   "eu-es", "fa", "fa-AF", "fi", "fi-FI", "fil", "fil-ph", "fo",
+                   "fo-dk", "fr", "fr-002", "fr-be", "fr-ca", "fr-ch", "fr-ci",
+                   "fr-fr", "fr-it", "fr-lu", "fr-ma", "ga", "ga-ie", "gl",
+                   "gl-es", "gu", "gu-in", "he", "he-IL", "hi", "hi-in", "hr",
+                   "hr-HR", "hu", "hu-hu", "hu-ro", "hu-sk", "hu-ua", "hy",
+                   "id", "id-id", "ikt", "is", "it", "it-ch", "it-it", "iu",
+                   "ja", "ja-JP", "jv-id", "ka", "kk", "kl-dk", "km", "kn",
+                   "kn-in", "ko", "ko-kr", "ks-in", "ku", "ky", "lt", "lt-lt",
+                   "lt-lv", "lv", "lv-lv", "mi-nz", "mk", "ml", "ml-in", "mn",
+                   "mr", "mr-in", "ms", "ms-my", "mt", "my", "nb", "nb-no",
+                   "ne", "nl", "nl-be", "nl-id", "nl-nl", "nn", "nn-no", "no",
+                   "nso-za", "or-in", "pa", "pa-in", "pl", "pl-cz", "pl-lt",
+                   "pl-pl", "pl-ua", "ps", "ps-AF", "pt", "pt-br", "pt-pt",
+                   "qu-bo", "qu-ec", "qu-pe", "rn-BI", "ro", "ro-bg", "ro-ro",
+                   "ro-sk", "ro-ua", "ru", "ru-ee", "ru-kz", "ru-lt", "ru-lv",
+                   "ru-ua", "sa-in", "sd-in", "si", "sk", "sk-cz", "sk-sk",
+                   "sl", "sl-SI", "so", "sq", "sr", "sr-me", "st-za", "sv",
+                   "sv-se", "sw", "sw-RW", "ta", "ta-in", "te", "te-in", "tg",
+                   "th", "tk", "tl", "tn-za", "tr", "tr-bg", "ts-za", "uk",
+                   "uk-sk", "uk-ua", "ur", "ur-in", "ur-PK", "uz", "vi",
+                   "vi-vn", "xh", "xh-za", "zh-cn", "zh-hk", "zh-mo", "zh-sg",
+                   "zh-tw", "zu-za"]
+      languages.map do |lang|
+        double('AppFile', relative_path: "translations/#{lang}.json", read: read_fixture_file('valid_en.json'))
+      end
+    end
+    it 'should be valid' do
+      expect(subject).to be_empty
+    end
+  end
+
   context 'when there is file with invalid JSON' do
     let(:translation_files) do
       [double('AppFile', relative_path: 'translations/en.json', read: '}')]
