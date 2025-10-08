9f240261520836ae252f3dadc3af7a0c3f2fef03
zendesk-punavwalke <punav.walke@zendesk.com>
2025-10-08 14:35:53 +0530

    Add Custom Objects V2 validation error messages and remove unused constants

--- lib/zendesk_apps_support/app_requirement.rb
+++ lib/zendesk_apps_support/app_requirement.rb
@@ -7,9 +7,6 @@ module ZendeskAppsSupport
     CUSTOM_OBJECTS_TYPE_KEY = 'custom_object_types'
     CUSTOM_OBJECTS_RELATIONSHIP_TYPE_KEY = 'custom_object_relationship_types'
     CUSTOM_OBJECTS_V2_KEY = 'custom_objects_v2'
-    CUSTOM_OBJECTS_V2_OBJECTS_KEY = 'objects'
-    CUSTOM_OBJECTS_V2_OBJECT_FIELDS_KEY = 'object_fields'
-    CUSTOM_OBJECT_V2_OBJECT_TRIGGERS_KEY = 'object_triggers'
     TYPES = %w[automations channel_integrations custom_objects macros targets views ticket_fields
                triggers user_fields organization_fields webhooks custom_objects_v2].freeze
   end
