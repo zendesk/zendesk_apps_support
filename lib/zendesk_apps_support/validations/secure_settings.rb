adbf1721903eadd43a0deea1594272338e2a32d8
satish-bhamare <satish.bhamare@zendesk.com>
2025-11-24 19:29:41 +0530

    Warn app developer when no scopes are configured for secure settings

--- lib/zendesk_apps_support/validations/secure_settings.rb
+++ lib/zendesk_apps_support/validations/secure_settings.rb
@@ -15,6 +15,14 @@ module ZendeskAppsSupport
 
           secure_or_hidden_default_param_found = manifest_params.any? { |param| secure_or_hidden_default_param?(param) }
           package.warnings << hidden_default_parameter_warning if secure_or_hidden_default_param_found
+
+          secure_params_without_scopes = manifest_params.filter_map do |param|
+            next unless param.secure || param.name =~ SECURABLE_KEYWORDS_REGEXP
+
+            param.name if secure_param_without_scopes?(param)
+          end
+
+          package.warnings << no_scopes_warning(secure_params_without_scopes) if secure_params_without_scopes.any?
         end
 
         private
@@ -40,6 +48,18 @@ module ZendeskAppsSupport
         def secure_settings_warning
           I18n.t(
             'txt.apps.admin.error.app_build.translation.insecure_token_parameter_in_manifest',
+            link: 'https://developer.zendesk.com/apps/docs/developer-guide/using_sdk#using-secure-settings'
+          )
+        end
+
+        def secure_param_without_scopes?(parameter)
+          !parameter.scopes&.any?
+        end
+
+        def no_scopes_warning(param_names)
+          I18n.t(
+            'txt.apps.admin.error.app_build.translation.secure_parameters_with_no_scopes_in_manifest',
+            params: param_names.join(', '),
             link: 'https://developer.zendesk.com/apps/docs/developer-guide/using_sdk#using-secure-settings'
           )
         end
--- spec/validations/secure_settings_spec.rb
+++ spec/validations/secure_settings_spec.rb
@@ -4,12 +4,16 @@ require 'spec_helper'
 
 describe ZendeskAppsSupport::Validations::SecureSettings do
   let(:package) { double('Package', warnings: []) }
-  let(:secured_param)  { ZendeskAppsSupport::Manifest::Parameter.new('name' => 'my_token', 'secure' => true) }
-  let(:insecure_param) { ZendeskAppsSupport::Manifest::Parameter.new('name' => 'my_token') }
+  let(:secured_param_no_scopes) { ZendeskAppsSupport::Manifest::Parameter.new('name' => 'my_token', 'secure' => true) }
+  let(:secured_param_oauth_no_scopes) { ZendeskAppsSupport::Manifest::Parameter.new('name' => 'oauth_token', 'secure' => true) }
+  let(:secured_param_with_scopes) do
+    ZendeskAppsSupport::Manifest::Parameter.new('name' => 'token_with_scopes', 'secure' => true, 'scopes' => ['header'])
+  end
+  let(:insecure_param) { ZendeskAppsSupport::Manifest::Parameter.new('name' => 'my_insecure_token') }
   let(:regular_param)  { ZendeskAppsSupport::Manifest::Parameter.new('name' => 'subdomain') }
   let(:regular_default_param)  { ZendeskAppsSupport::Manifest::Parameter.new('name' => 'subdomain', 'default' => true) }
   let(:secured_default_param)  do
-    ZendeskAppsSupport::Manifest::Parameter.new('name' => 'subdomain',
+    ZendeskAppsSupport::Manifest::Parameter.new('name' => 'secured_default_subdomain',
                                                 'secure' => true,
                                                 'default' => true)
   end
@@ -33,8 +37,9 @@ describe ZendeskAppsSupport::Validations::SecureSettings do
       allow(package).to receive_message_chain('manifest.parameters') { [secured_default_param] }
       subject.call(package)
 
-      expect(package.warnings.size).to eq(1)
+      expect(package.warnings.size).to eq(2)
       expect(package.warnings[0]).to include('confirm they do not contain sensitive data')
+      expect(package.warnings[1]).to include('Make sure to set scopes for secure Settings: secured_default_subdomain')
     end
   end
 
@@ -62,12 +67,45 @@ describe ZendeskAppsSupport::Validations::SecureSettings do
       allow(package).to receive_message_chain('manifest.parameters') { [ insecure_param, regular_param ] }
       subject.call(package)
 
-      expect(package.warnings.size).to eq(1)
+      expect(package.warnings.size).to eq(2)
       expect(package.warnings[0]).to include('Make sure to set secure to true')
+      expect(package.warnings[1]).to eq('Make sure to set scopes for secure Settings: my_insecure_token')
     end
 
-    it 'returns no warning if secure key is true' do
-      allow(package).to receive_message_chain('manifest.parameters') { [ secured_param, regular_param ] }
+    it 'returns warning only for parameters that are secure but with no scopes' do
+      allow(package).to receive_message_chain('manifest.parameters') do
+        [secured_param_no_scopes, secured_param_oauth_no_scopes, secured_param_with_scopes, regular_param]
+      end
+      subject.call(package)
+
+      expect(package.warnings.size).to eq(1) 
+      expect(package.warnings[0]).to eq('Make sure to set scopes for secure Settings: my_token, oauth_token')
+    end
+
+    it 'show warning for secured, insecured, default parameters' do
+      allow(package).to receive_message_chain('manifest.parameters') do
+        [
+          secured_param_no_scopes,
+          secured_param_oauth_no_scopes,
+          secured_param_with_scopes,
+          insecure_param,
+          regular_param,
+          regular_default_param,
+          secured_default_param
+        ]
+      end
+      subject.call(package)
+
+      expect(package.warnings.size).to eq(3)
+      expect(package.warnings[0]).to include('Make sure to set secure to true when using keys in Settings')
+      expect(package.warnings[1]).to include('Default values for secure or hidden parameters are not stored securely')
+      expect(package.warnings[2]).to eq(
+        'Make sure to set scopes for secure Settings: my_token, oauth_token, my_insecure_token, secured_default_subdomain'
+      )
+    end
+
+    it 'returns no warning' do
+      allow(package).to receive_message_chain('manifest.parameters') { [ secured_param_with_scopes, regular_param ] }
       subject.call(package)
 
       expect(package.warnings).to be_empty
